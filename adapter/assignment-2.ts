cb7783c5e0b61e52e94ed887fe49e08e2cad814a
James Barbour <jameskbarbour1@gmail.com>
2025-05-17 13:17:05 +0000

    Changed to import assignment-2

--- adapter/assignment-2.ts
+++ adapter/assignment-2.ts
@@ -1,4 +1,6 @@
 import assignment1 from "./assignment-1";
+import books from'./../mcmasteful-book-list.json';
+import { v4 as uuidv4 } from 'uuid';
 
 export type BookID = string;
 
@@ -11,12 +13,29 @@ export interface Book {
     image: string,
 };
 
+export const booksArray: Book[] = books; //Clears typescript id doesn't exist error
+
+booksArray.forEach((book) => {
+    if (!book.id) {
+        book.id = uuidv4(); // Assign a uuid if missing
+    }
+});
+
 async function listBooks(filters?: Array<{from?: number, to?: number}>) : Promise<Book[]>{
     return assignment1.listBooks(filters);
 }
 
 async function createOrUpdateBook(book: Book): Promise<BookID> {
-    throw new Error("Todo")
+   const bookIndex = booksArray.findIndex(b => b.id === book.id);
+    
+   if (bookIndex !== -1) {
+        // Update the existing book
+        booksArray[bookIndex] = { ...booksArray[bookIndex], ...book };
+    } else {
+        // Add new book
+        booksArray.push(book);
+    }
+    return book.id as BookID;
 }
 
 async function removeBook(book: BookID): Promise<void> {
