02f6dcf5ca45d1a08bfa9bd45c0bef97345f9ba5
James Barbour <jameskbarbour1@gmail.com>
2025-05-18 12:37:43 +0000

    Initial commit

---
+++ src/server/routes.ts
@@ -0,0 +1,142 @@
+import Router from 'koa-router'; 
+import { Book } from '../../adapter/assignment-2';
+import { v4 as uuidv4 } from 'uuid'; 
+import assignment2Functions from '../../adapter/assignment-2';
+
+const { createOrUpdateBook, removeBook, listBooks } = assignment2Functions;
+
+const router = new Router();
+
+//Create a book
+router.post('/books', async (ctx) => {
+    try {
+        const { name, author, description, price, image } = ctx.request.body as Book 
+        //Validate user inputs
+         if(!name || !author || !description || !price || !image) {
+            ctx.status = 400;
+            return ctx.body = { error: 'All fields are required' };
+        }
+
+        //Extra validation for price
+        if(isNaN(price)  || price < 0) {
+            ctx.status = 400;
+            return ctx.body = { error: 'Price must be a positive number' }
+        }
+
+        const newBook: Book = {
+            id: uuidv4(), //I used uuids because we did in BDV102
+            name,
+            author,
+            description,
+            price: Number(price),
+            image
+        }
+
+        await createOrUpdateBook(newBook);
+        ctx.status = 201;
+        ctx.body = { message: 'New book created successfully', book: newBook };
+
+    } catch (error) {
+        ctx.status = 500;
+        console.error('An error occurred:', error);
+        ctx.body = { error: 'Server error, please try again' }
+    }
+})
+
+
+//Update a book
+router.put('/books/:id', async (ctx) => {
+    try {
+        const { name, author, description, price, image } = ctx.request.body as Book;
+        //Validate user inputs
+         if(!name || !author || !description || !price || !image) {
+            ctx.status = 400;
+            return ctx.body = { error: 'All fields are required' };
+        }
+        
+        //Extra validation for price
+        if(isNaN(price)  || price < 0) {
+            ctx.status = 400;
+            return ctx.body = { error: 'Price must be a positive number' }
+        }
+      
+        //Validate id
+        const { id } = ctx.params;
+        if (!id) {
+            ctx.status = 400;
+            return ctx.body = { error: 'Book ID is required' };
+        }
+
+        const updatedBook: Book = {
+            id,
+            name,
+            author,
+            description,
+            price: Number(price),
+            image,
+        };
+
+        await createOrUpdateBook(updatedBook);
+        ctx.status = 200;
+        ctx.body = { message: 'Book updated successfully', book: updatedBook };
+
+    } catch (error) {
+        ctx.status = 500;
+        console.error('An error occurred:', error);
+        ctx.body = { error: 'Server error, please try again' };
+    }
+});
+
+//Delete
+router.delete('/books/:id', async (ctx) => {
+    try {
+        const { id } = ctx.params;
+        if (!id) {
+            ctx.status = 400;
+            return ctx.body = { error: 'Book ID is required' };
+        }
+
+        await removeBook(id);
+        ctx.status = 200;
+        ctx.body = { message: 'Book deleted successfully' };
+
+    } catch (error) {
+        ctx.status = 500;
+        console.error('An error occurred:', error);
+        ctx.body = { error: 'Server error, please try again' };
+    }
+});
+
+//Read
+router.get('/books', async (ctx) => {
+    const filters = ctx.query.filters as Array<{ from?: number, to?: number }> | undefined;
+
+    try {
+        // Validate filters if used
+        if (filters && filters.length > 0) {
+            const isValid = filters.every(filter => {
+                const from = filter.from;
+                const to = filter.to;
+                return (
+                    (from === undefined || !isNaN(Number(from))) &&
+                    (to === undefined || !isNaN(Number(to)))
+                );
+            });
+            // If user tries NaN input
+            if (!isValid) {
+                ctx.status = 400;
+                ctx.body = { error: "Invalid filters: 'from' and 'to' must be valid numbers." };
+                return;
+            }
+        }
+
+        const filteredBooks = await listBooks(filters)
+        ctx.body = filteredBooks;
+    } catch (error) {
+        ctx.status = 500;
+        console.error('An error occurred:', error);
+        ctx.body = { error: 'Server error, please try again' };
+    }
+});
+
+export default router;
